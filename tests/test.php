5722520d0584adc0b42eefd2a4e03343c5b12dba
Fabien MARTY <fab@php.net>
2002-07-26 07:52:16 +0000

    Adding testing stuff
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@89848 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/test.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('../Cache_Lite.php');
+
+$options = array(
+    'cacheDir' => '/tmp/',
+    'lifeTime' => 10
+);
+
+$Cache_Lite = new Cache_Lite($options);
+
+if ($data = $Cache_Lite->get('123')) {
+    echo('Cache Hit !<br>');
+    echo($data);
+} else {
+    echo('Cache Missed !<br>');
+    $data = '';
+    for($i=0;$i<1000;$i++) {
+        $data .= '0123456789';
+    }
+    echo($data);
+    $Cache_Lite->save('123', $data);
+}
+
+?>
