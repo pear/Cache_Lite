995201ab172535271e27366fae4d5d4887e492be
Fabien MARTY <fab@php.net>
2002-08-06 14:48:40 +0000

    Adding of a test file for Cache_Lite_Output
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@91083 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/test2.php
@@ -0,0 +1,26 @@
+<?php
+
+// Test script of Cache_Lite_Output
+// $Id$
+
+require_once('../Cache_Lite_Output.php');
+
+$options = array(
+    'cacheDir' => '/tmp/',
+    'lifeTime' => 10
+);
+
+$cache = new Cache_Lite_Output($options);
+
+if (!($cache->start('123'))) {
+    // Cache missed...
+    for($i=0;$i<1000;$i++) { // Making of the page...
+        echo('0123456789');
+    }
+    $cache->end();
+    echo('<br>Cache Missed !');
+} else {
+    echo('<br>Cache Hit !');
+}
+
+?>
