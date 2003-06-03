c548d5c6ca6b4e4c992ac9f22cb4d310518598da
Fabien MARTY <fab@php.net>
2003-06-03 10:14:36 +0000

    it's better with a ';' at the end of the line !!! :))
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@129936 c90b9560-bf6c-de11-be94-00142212c4b1

--- Lite/Function.php
+++ Lite/Function.php
@@ -71,7 +71,7 @@ class Cache_Lite_Function extends Cache_Lite
         $arguments = func_get_args();
         $id = serialize($arguments); // Generate a cache id
         if (!$this->_fileNameProtection) {
-            $id = md5($id)
+            $id = md5($id);
             // if fileNameProtection is set to false, then the id has to be hashed
             // because it's a very bad file name in most cases
         }
