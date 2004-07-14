37d248d96237a066eb895b84c312a6f4fcb1ecfc
Fabien MARTY <fab@php.net>
2004-07-14 10:24:03 +0000

    Bug fix #1758, thanks to Dave <djpenton at cs dot mu dot oz dot au>
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@163290 c90b9560-bf6c-de11-be94-00142212c4b1

--- Lite.php
+++ Lite.php
@@ -268,7 +268,7 @@ class Cache_Lite
                     $data = $this->_read();
                 }
             } else {
-                if (@filemtime($this->_file) > $this->_refreshTime) {
+                if ((file_exists($this->_file)) && (@filemtime($this->_file) > $this->_refreshTime)) {
                     $data = $this->_read();
                 }
             }
