d458bb04114e798238c85214d422dbe0fbe3e7e7
Markus Tacker <tacker@php.net>
2009-07-07 05:34:37 +0000

    Set magic quotes runtime only if needed. See Pear Bug #15725.
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@283629 c90b9560-bf6c-de11-be94-00142212c4b1

--- Lite.php
+++ Lite.php
@@ -712,7 +712,9 @@ class Cache_Lite
             clearstatcache();
             $length = @filesize($this->_file);
             $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
+            if ($mqr) {
+                set_magic_quotes_runtime(0);
+            }
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length = $length - 32;
@@ -722,7 +724,9 @@ class Cache_Lite
             } else {
                 $data = '';
             }
-            set_magic_quotes_runtime($mqr);
+            if ($mqr) {
+                set_magic_quotes_runtime($mqr);
+            }
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
@@ -767,9 +771,13 @@ class Cache_Lite
                 @fwrite($fp, $this->_hash($data, $this->_readControlType), 32);
             }
             $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
+            if ($mqr) {
+                set_magic_quotes_runtime(0);
+            }
             @fwrite($fp, $data);
-            set_magic_quotes_runtime($mqr);
+            if ($mqr) {
+                set_magic_quotes_runtime($mqr);
+            }
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             return true;
