9da9753b15245d662e73a03fa36c49aa472937fa
Markus Tacker <tacker@php.net>
2011-02-07 10:43:57 +0000

    Added script for testing Bug #18192.
    git-svn-id: http://svn.php.net/repository/pear/packages/Cache_Lite/trunk@308092 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/pearbug18192.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Process for test pearbug18192
+ * 
+ * @see https://pear.php.net/bugs/bug.php?id=18192
+ * @author Markus Tacker <tacker@php.net>
+ */
+
+require_once 'Cache/Lite.php'; 
+$c = new Cache_Lite(array('cacheDir' => '.', 'lifeTime' => 60,)); 
+$id = '#18192'; 
+for ($i = 0; $i < 100000; $i++) {
+	$str = uniqid('some-string', true); 
+	if (!$c->save($str, $id)) fputs(STDERR, "Error saving.\n");
+	if ($c->get($id) !== $str) fputs(STDERR, "Wrong data.\n");
+}
