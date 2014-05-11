9fa08bd625f86946d41a6e7ff66f6f4ea5c0f0bb
Markus Tacker <m@coderbyheart.de>
2014-05-11 17:02:19 +0200

    Do not report E_STRICT errors on PHP5.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
+if (version_compare(PHP_VERSION, '5.4', '>=')) {
     error_reporting(E_ALL ^ E_STRICT);
+} else if (version_compare(PHP_VERSION, '5.3', '>=')) {
+    error_reporting(E_ALL & ~E_STRICT);
+}
